a420821178ccf9e9173e9f63415e667deff02c05
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2019-04-01 00:36:07 +0900

    [compat] add ROS2Transport

---
+++ OpenRTM_aist/ext/transport/ROS2Transport/ROS2MessageInfo.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python
+# -*- coding: euc-jp -*-
+
+##
+# @file ROS2MessageInfo.py
+# @brief ROS2 Message Info class
+# @date $Date$
+# @author Nobuhiko Miyamoto <n-miyamoto@aist.go.jp>
+#
+# Copyright (C) 2019
+#     Noriaki Ando
+#     Robot Innovation Research Center,
+#     National Institute of
+#         Advanced Industrial Science and Technology (AIST), Japan
+#     All rights reserved.
+#
+# $Id$
+#
+
+import OpenRTM_aist
+
+##
+# @if jp
+# @class ROS2MessageInfoBase
+# @brief ROS2メッセージ情報格納オブジェクトの基底クラス
+# ROS2データ型名、IDLファイルパスを登録する
+#
+# @else
+# @class ROS2OutPort
+# @brief 
+#
+#
+# @endif
+class ROS2MessageInfoBase(object):
+  ##
+  # @if jp
+  # @brief コンストラクタ
+  #
+  # コンストラクタ
+  #
+  # @param self
+  #
+  # @else
+  # @brief Constructor
+  #
+  # @param self
+  #
+  # @endif
+  def __init__(self):
+    pass
+  ##
+  # @if jp
+  # @brief デストラクタ
+  #
+  # デストラクタ
+  #
+  # @param self
+  #
+  # @else
+  # @brief Destructor
+  #
+  # Destructor
+  #
+  # @param self
+  #
+  # @endif
+  #
+  def __del__(self):
+    pass
+
+  ##
+  # @if jp
+  # @brief データの型名を取得
+  #
+  # @param self
+  # @return 型名
+  #
+  # @else
+  # @brief 
+  #
+  #
+  # @param self
+  # @return
+  #
+  # @endif
+  #
+  def datatype(self):
+    return None
+
+
+
+  
+
+##
+# @if jp
+# @brief メッセージの情報格納オブジェクト生成関数
+#
+# @param data_class ROS2データ型
+# @return メッセージの情報格納オブジェクト
+#
+# @else
+# @brief 
+#
+# @param data_class 
+# @return 
+#
+# @endif
+#
+def ros2_message_info(datatype):
+  ##
+  # @if jp
+  # @class ROS2MessageInfo
+  # @brief メッセージの情報格納クラス
+  #
+  #
+  # @else
+  # @class ROS2MessageInfo
+  # @brief 
+  #
+  #
+  # @endif
+  class ROS2MessageInfo(ROS2MessageInfoBase):
+    """
+    """
+
+    ##
+    # @if jp
+    # @brief コンストラクタ
+    #
+    # コンストラクタ
+    #
+    # @param self
+    #
+    # @else
+    # @brief Constructor
+    #
+    # @param self
+    #
+    # @endif
+    def __init__(self):
+      super(ROS2MessageInfo, self).__init__()
+      
+
+    ##
+    # @if jp
+    # @brief デストラクタ
+    #
+    #
+    # @param self
+    #
+    # @else
+    #
+    # @brief self
+    #
+    # @endif
+    def __del__(self):
+      pass
+
+    ##
+    # @if jp
+    # @brief メッセージの型名を取得
+    #
+    # @param self
+    # @return 型名
+    #
+    # @else
+    # @brief 
+    #
+    #
+    # @param self
+    # @return
+    #
+    # @endif
+    #
+    def datatype(self):
+      return datatype
+
+
+  return ROS2MessageInfo
+
+
+
+ros2messageinfofactory = None
+
+
+##
+# @if jp
+# @class ROS2MessageInfoFactory
+# @brief ROS2メッセージ情報格納オブジェクト生成ファクトリ
+#
+# @else
+# @class ROS2MessageInfoFactory
+# @brief 
+#
+#
+# @endif
+class ROS2MessageInfoFactory(OpenRTM_aist.Factory,ROS2MessageInfoBase):
+  ##
+  # @if jp
+  # @brief コンストラクタ
+  #
+  # コンストラクタ
+  #
+  # @param self
+  #
+  # @else
+  # @brief Constructor
+  #
+  # @param self
+  #
+  # @endif
+  def __init__(self):
+    OpenRTM_aist.Factory.__init__(self)
+
+  ##
+  # @if jp
+  # @brief デストラクタ
+  #
+  # デストラクタ
+  #
+  # @param self
+  #
+  # @else
+  # @brief Destructor
+  #
+  # Destructor
+  #
+  # @param self
+  #
+  # @endif
+  #
+  def __del__(self):
+    pass
+
+  ##
+  # @if jp
+  # @brief インスタンス取得
+  #
+  #
+  # @return インスタンス
+  #
+  # @else
+  # @brief 
+  #
+  #
+  # @return
+  #
+  # @endif
+  #
+  def instance():
+    global ros2messageinfofactory
+
+    if ros2messageinfofactory is None:
+      ros2messageinfofactory = ROS2MessageInfoFactory()
+
+    return ros2messageinfofactory
+
+  instance = staticmethod(instance)
+
---
+++ OpenRTM_aist/ext/transport/ROS2Transport/ROS2Serializer.py
@@ -0,0 +1,870 @@
+#!/usr/bin/env python
+# -*- coding: euc-jp -*-
+
+##
+# @file ROS2Serializer.py
+# @brief ROS2 Serializer class
+# @date $Date$
+# @author Nobuhiko Miyamoto <n-miyamoto@aist.go.jp>
+#
+# Copyright (C) 2019
+#     Noriaki Ando
+#     Robot Innovation Research Center,
+#     National Institute of
+#         Advanced Industrial Science and Technology (AIST), Japan
+#     All rights reserved.
+#
+# $Id$
+#
+
+import OpenRTM_aist
+import omniORB
+
+import struct
+import ROS2MessageInfo
+import RTC
+import rclpy
+from std_msgs.msg import Float32
+from std_msgs.msg import Float64
+from std_msgs.msg import Int8
+from std_msgs.msg import Int16
+from std_msgs.msg import Int32
+from std_msgs.msg import Int64
+from std_msgs.msg import UInt8
+from std_msgs.msg import UInt16
+from std_msgs.msg import UInt32
+from std_msgs.msg import UInt64
+from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float64MultiArray
+from std_msgs.msg import Int8MultiArray
+from std_msgs.msg import Int16MultiArray
+from std_msgs.msg import Int32MultiArray
+from std_msgs.msg import Int64MultiArray
+from std_msgs.msg import UInt8MultiArray
+from std_msgs.msg import UInt16MultiArray
+from std_msgs.msg import UInt32MultiArray
+from std_msgs.msg import UInt64MultiArray
+from std_msgs.msg import String
+from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import QuaternionStamped
+from geometry_msgs.msg import Vector3Stamped
+from sensor_msgs.msg import Image
+
+
+
+
+
+##
+# @if jp
+# @brief 単一データ、配列などの基本メッセージ型のROS2シリアライザの生成関数
+#
+# @param message_type ROS2メッセージ型
+#
+# @else
+# @brief 
+#
+# @param message_type 
+#
+# @endif
+#
+def ros2_basic_data(message_type):
+  ##
+  # @if jp
+  # @class ROS2BasicData
+  # @brief 単一データ、配列などの基本メッセージ型
+  #
+  # @else
+  # @class ROS2BasicData
+  # @brief 
+  #
+  #
+  # @endif
+  class ROS2BasicData(OpenRTM_aist.ByteDataStreamBase):
+    """
+    """
+
+    ##
+    # @if jp
+    # @brief コンストラクタ
+    #
+    # コンストラクタ
+    #
+    # @param self
+    #
+    # @else
+    # @brief Constructor
+    #
+    # @param self
+    #
+    # @endif
+    def __init__(self):
+      pass
+
+    ##
+    # @if jp
+    # @brief デストラクタ
+    #
+    #
+    # @param self
+    #
+    # @else
+    #
+    # @brief self
+    #
+    # @endif
+    def __del__(self):
+      pass
+
+    ##
+    # @if jp
+    # @brief 設定初期化
+    #
+    # 
+    # @param prop 設定情報
+    #
+    # @else
+    #
+    # @brief Initializing configuration
+    #
+    #
+    # @param prop Configuration information
+    #
+    # @endif
+    ## virtual ReturnCode init(coil::Properties& prop) = 0;
+    def init(self, prop):
+      pass
+
+
+    ##
+    # @if jp
+    # @brief データの変換(omniORB->ROS2)
+    #
+    # 
+    # @param data omniORBのデータ
+    # @return ret、value
+    # ret：SERIALIZE_OK：成功、SERIALIZE_ERROR：失敗、SERIALIZE_NOTFOUND：指定のシリアライザがない
+    # value：ROS2のデータ
+    #
+    # @else
+    #
+    # @brief 
+    #
+    #
+    # @param data 
+    # @return
+    #
+    # @endif
+    def serialize(self, data):
+      msg = message_type()
+      field_type = msg.get_fields_and_field_types()["data"]
+      if field_type == "int8" or field_type == "int16" or field_type == "int32" or field_type == "int64":
+          msg.data = int(data.data)
+      elif field_type == "uint8" or field_type == "uint16" or field_type == "uint32" or field_type == "uint64":
+          msg.data = int(data.data)
+      elif field_type == "float32" or field_type == "float64":
+          msg.data = float(data.data)
+      elif field_type == "int8[]" or field_type == "int16[]" or field_type == "int32[]" or field_type == "int64[]":
+          msg.data = list(map(int, data.data))
+      elif field_type == "uint8[]" or field_type == "uint16[]" or field_type == "uint32[]" or field_type == "uint64[]":
+          msg.data = list(map(int, data.data))
+      elif field_type == "float32[]" or field_type == "float64[]":
+          msg.data = list(map(float, data.data))
+      else: 
+          msg.data = data.data
+
+      return OpenRTM_aist.ByteDataStreamBase.SERIALIZE_OK, msg
+
+    ##
+    # @if jp
+    # @brief データの変換(ROS2->omniORB)
+    #
+    # @param self 
+    # @param bdata ROS2のデータ
+    # @param data_type omniORBのデータ
+    # @return ret、value
+    # ret：SERIALIZE_OK：成功、SERIALIZE_ERROR：失敗、SERIALIZE_NOTFOUND：指定のシリアライザがない
+    # value：変換後のデータ
+    #
+    # @else
+    #
+    # @brief 
+    #
+    # @param cdr
+    # @param data_type 
+    # @return 
+    #
+    # @endif
+    def deserialize(self, bdata, data_type):
+      try:
+        data_type.data = bdata.data
+        return OpenRTM_aist.ByteDataStreamBase.SERIALIZE_OK, data_type
+      except:
+        return OpenRTM_aist.ByteDataStreamBase.SERIALIZE_NOT_SUPPORT_ENDIAN, data_type
+  return ROS2BasicData
+
+##
+# @if jp
+# @brief 単一データ、配列などの基本メッセージ型のシリアライザの初期化
+#
+# @param message_type ROSメッセージ型
+# @param name シリアライザの名前
+#
+# @else
+# @brief 
+#
+# @param message_type 
+# @param name 
+#
+#
+# @endif
+#
+def ROS2BasicDataInit(message_type, name):
+  OpenRTM_aist.SerializerFactory.instance().addFactory(name,
+                                                      ros2_basic_data(message_type),
+                                                      OpenRTM_aist.Delete)
+  ROS2MessageInfo.ROS2MessageInfoFactory.instance().addFactory(name,
+                                                      ROS2MessageInfo.ros2_message_info(message_type),
+                                                      OpenRTM_aist.Delete)
+
+
+
+##
+# @if jp
+# @class ROS2Point3DData
+# @brief PointStamped型のシリアライザ初期化
+#
+# @else
+# @class ROS2Point3DData
+# @brief 
+#
+#
+# @endif
+class ROS2Point3DData(OpenRTM_aist.ByteDataStreamBase):
+  """
+  """
+
+  ##
+  # @if jp
+  # @brief コンストラクタ
+  #
+  # コンストラクタ
+  #
+  # @param self
+  #
+  # @else
+  # @brief Constructor
+  #
+  # @param self
+  #
+  # @endif
+  def __init__(self):
+    pass
+
+  ##
+  # @if jp
+  # @brief デストラクタ
+  #
+  #
+  # @param self
+  #
+  # @else
+  #
+  # @brief self
+  #
+  # @endif
+  def __del__(self):
+    pass
+
+  ##
+  # @if jp
+  # @brief 設定初期化
+  #
+  # 
+  # @param prop 設定情報
+  #
+  # @else
+  #
+  # @brief Initializing configuration
+  #
+  #
+  # @param prop Configuration information
+  #
+  # @endif
+  ## virtual ReturnCode init(coil::Properties& prop) = 0;
+  def init(self, prop):
+    pass
+
+
+  ##
+  # @if jp
+  # @brief データの変換(omniORB->ROS2)
+  #
+  # 
+  # @param data omniORBのデータ
+  # @return ret、value
+  # ret：SERIALIZE_OK：成功、SERIALIZE_ERROR：失敗、SERIALIZE_NOTFOUND：指定のシリアライザがない
+  # value：ROS2のデータ
+  #
+  # @else
+  #
+  # @brief 
+  #
+  #
+  # @param data 
+  # @return
+  #
+  # @endif
+  def serialize(self, data):
+    msg = PointStamped()
+    msg.header.stamp.secs = data.tm.sec
+    msg.header.stamp.nsecs = data.tm.nsec
+    msg.point.x = data.data.x
+    msg.point.y = data.data.y
+    msg.point.z = data.data.z
+
+    return OpenRTM_aist.ByteDataStreamBase.SERIALIZE_OK, msg
+
+  ##
+  # @if jp
+  # @brief データの変換(ROS2->omniORB)
+  #
+  # @param self 
+  # @param bdata ROS2のデータ
+  # @param data_type omniORBのデータ
+  # @return ret、value
+  # ret：SERIALIZE_OK：成功、SERIALIZE_ERROR：失敗、SERIALIZE_NOTFOUND：指定のシリアライザがない
+  # value：変換後のデータ
+  #
+  # @else
+  #
+  # @brief 
+  #
+  # @param cdr
+  # @param data_type 
+  # @return 
+  #
+  # @endif
+  def deserialize(self, bdata, data_type):
+    try:
+      data_type.tm.sec = bdata.header.stamp.secs
+      data_type.tm.nsec = bdata.header.stamp.nsecs
+      data_type.data.x = bdata.point.x
+      data_type.data.y = bdata.point.y
+      data_type.data.z = bdata.point.z
+      return OpenRTM_aist.ByteDataStreamBase.SERIALIZE_OK, data_type
+    except:
+      return OpenRTM_aist.ByteDataStreamBase.SERIALIZE_NOT_SUPPORT_ENDIAN, data_type
+
+
+##
+# @if jp
+# @brief PointStamped型のシリアライザの初期化
+#
+#
+# @else
+# @brief 
+#
+#
+# @endif
+#
+def ROS2Point3DInit():
+  OpenRTM_aist.SerializerFactory.instance().addFactory("ROS2PointStamped",
+                                                      ROS2Point3DData,
+                                                      OpenRTM_aist.Delete)
+  ROS2MessageInfo.ROS2MessageInfoFactory.instance().addFactory("ROS2PointStamped",
+                                                      ROS2MessageInfo.ros2_message_info(PointStamped),
+                                                      OpenRTM_aist.Delete)
+
+
+##
+# @if jp
+# @class ROS2QuaternionData
+# @brief QuaternionStamped型のシリアライザ
+#
+# @else
+# @class ROS2QuaternionData
+# @brief 
+#
+#
+# @endif
+class ROS2QuaternionData(OpenRTM_aist.ByteDataStreamBase):
+  """
+  """
+
+  ##
+  # @if jp
+  # @brief コンストラクタ
+  #
+  # コンストラクタ
+  #
+  # @param self
+  #
+  # @else
+  # @brief Constructor
+  #
+  # @param self
+  #
+  # @endif
+  def __init__(self):
+    pass
+
+  ##
+  # @if jp
+  # @brief デストラクタ
+  #
+  #
+  # @param self
+  #
+  # @else
+  #
+  # @brief self
+  #
+  # @endif
+  def __del__(self):
+    pass
+
+  ##
+  # @if jp
+  # @brief 設定初期化
+  #
+  # 
+  # @param prop 設定情報
+  #
+  # @else
+  #
+  # @brief Initializing configuration
+  #
+  #
+  # @param prop Configuration information
+  #
+  # @endif
+  ## virtual ReturnCode init(coil::Properties& prop) = 0;
+  def init(self, prop):
+    pass
+
+
+  ##
+  # @if jp
+  # @brief データの変換(omniORB->ROS2)
+  #
+  # 
+  # @param data omniORBのデータ
+  # @return ret、value
+  # ret：SERIALIZE_OK：成功、SERIALIZE_ERROR：失敗、SERIALIZE_NOTFOUND：指定のシリアライザがない
+  # value：ROS2のデータ
+  #
+  # @else
+  #
+  # @brief 
+  #
+  #
+  # @param data 
+  # @return
+  #
+  # @endif
+  def serialize(self, data):
+    msg = QuaternionStamped()
+    msg.header.stamp.secs = data.tm.sec
+    msg.header.stamp.nsecs = data.tm.nsec
+    msg.quaternion.x = data.data.x
+    msg.quaternion.y = data.data.y
+    msg.quaternion.z = data.data.z
+    msg.quaternion.w = data.data.w
+
+    return OpenRTM_aist.ByteDataStreamBase.SERIALIZE_OK, msg
+
+  ##
+  # @if jp
+  # @brief データの変換(ROS2->omniORB)
+  #
+  # @param self 
+  # @param bdata ROS2のデータ
+  # @param data_type omniORBのデータ
+  # @return ret、value
+  # ret：SERIALIZE_OK：成功、SERIALIZE_ERROR：失敗、SERIALIZE_NOTFOUND：指定のシリアライザがない
+  # value：変換後のデータ
+  #
+  # @else
+  #
+  # @brief 
+  #
+  # @param cdr
+  # @param data_type 
+  # @return 
+  #
+  # @endif
+  def deserialize(self, bdata, data_type):
+    try:
+      data_type.tm.sec = bdata.header.stamp.secs
+      data_type.tm.nsec = bdata.header.stamp.nsecs
+      data_type.data.x = bdata.quaternion.x
+      data_type.data.y = bdata.quaternion.y
+      data_type.data.z = bdata.quaternion.z
+      data_type.data.w = bdata.quaternion.w
+      return OpenRTM_aist.ByteDataStreamBase.SERIALIZE_OK, data_type
+    except:
+      return OpenRTM_aist.ByteDataStreamBase.SERIALIZE_NOT_SUPPORT_ENDIAN, data_type
+
+
+##
+# @if jp
+# @brief QuaternionStamped型のシリアライザの初期化
+#
+#
+# @else
+# @brief 
+#
+#
+# @endif
+#
+def ROS2QuaternionInit():
+  OpenRTM_aist.SerializerFactory.instance().addFactory("ROS2QuaternionStamped",
+                                                      ROS2QuaternionData,
+                                                      OpenRTM_aist.Delete)
+  ROS2MessageInfo.ROS2MessageInfoFactory.instance().addFactory("ROS2QuaternionStamped",
+                                                      ROS2MessageInfo.ros2_message_info(QuaternionStamped),
+                                                      OpenRTM_aist.Delete)
+
+
+
+##
+# @if jp
+# @class ROS2Vector3DData
+# @brief Vector3Stamped型のシリアライザ
+#
+# @else
+# @class ROS2Vector3DData
+# @brief 
+#
+#
+# @endif
+class ROS2Vector3DData(OpenRTM_aist.ByteDataStreamBase):
+  """
+  """
+
+  ##
+  # @if jp
+  # @brief コンストラクタ
+  #
+  # コンストラクタ
+  #
+  # @param self
+  #
+  # @else
+  # @brief Constructor
+  #
+  # @param self
+  #
+  # @endif
+  def __init__(self):
+    pass
+
+  ##
+  # @if jp
+  # @brief デストラクタ
+  #
+  #
+  # @param self
+  #
+  # @else
+  #
+  # @brief self
+  #
+  # @endif
+  def __del__(self):
+    pass
+
+  ##
+  # @if jp
+  # @brief 設定初期化
+  #
+  # 
+  # @param prop 設定情報
+  #
+  # @else
+  #
+  # @brief Initializing configuration
+  #
+  #
+  # @param prop Configuration information
+  #
+  # @endif
+  ## virtual ReturnCode init(coil::Properties& prop) = 0;
+  def init(self, prop):
+    pass
+
+
+  ##
+  # @if jp
+  # @brief データの変換(omniORB->ROS2)
+  #
+  # 
+  # @param data omniORBのデータ
+  # @return ret、value
+  # ret：SERIALIZE_OK：成功、SERIALIZE_ERROR：失敗、SERIALIZE_NOTFOUND：指定のシリアライザがない
+  # value：ROS2のデータ
+  #
+  # @else
+  #
+  # @brief 
+  #
+  #
+  # @param data 
+  # @return
+  #
+  # @endif
+  def serialize(self, data):
+    msg = Vector3Stamped()
+    msg.header.stamp.secs = data.tm.sec
+    msg.header.stamp.nsecs = data.tm.nsec
+    msg.vector.x = data.data.x
+    msg.vector.y = data.data.y
+    msg.vector.z = data.data.z
+
+    return OpenRTM_aist.ByteDataStreamBase.SERIALIZE_OK, msg
+
+  ##
+  # @if jp
+  # @brief データの変換(ROS2->omniORB)
+  #
+  # @param self 
+  # @param bdata ROS2のデータ
+  # @param data_type omniORBのデータ
+  # @return ret、value
+  # ret：SERIALIZE_OK：成功、SERIALIZE_ERROR：失敗、SERIALIZE_NOTFOUND：指定のシリアライザがない
+  # value：変換後のデータ
+  #
+  # @else
+  #
+  # @brief 
+  #
+  # @param cdr
+  # @param data_type 
+  # @return 
+  #
+  # @endif
+  def deserialize(self, bdata, data_type):
+    try:
+      data_type.tm.sec = bdata.header.stamp.secs
+      data_type.tm.nsec = bdata.header.stamp.nsecs
+      data_type.data.x = bdata.vector.x
+      data_type.data.y = bdata.vector.y
+      data_type.data.z = bdata.vector.z
+      return OpenRTM_aist.ByteDataStreamBase.SERIALIZE_OK, data_type
+    except:
+      return OpenRTM_aist.ByteDataStreamBase.SERIALIZE_NOT_SUPPORT_ENDIAN, data_type
+
+
+##
+# @if jp
+# @brief Vector3Stamped型のシリアライザの初期化
+#
+#
+# @else
+# @brief 
+#
+#
+# @endif
+#
+def ROS2Vector3DInit():
+  OpenRTM_aist.SerializerFactory.instance().addFactory("ROS2Vector3Stamped",
+                                                      ROS2Vector3DData,
+                                                      OpenRTM_aist.Delete)
+  ROS2MessageInfo.ROS2MessageInfoFactory.instance().addFactory("ROS2Vector3Stamped",
+                                                      ROS2MessageInfo.ros2_message_info(Vector3Stamped),
+                                                      OpenRTM_aist.Delete)
+
+
+##
+# @if jp
+# @class ROS2CameraImageData
+# @brief Image型のシリアライザ
+#
+# @else
+# @class ROS2CameraImageData
+# @brief 
+#
+#
+# @endif
+class ROS2CameraImageData(OpenRTM_aist.ByteDataStreamBase):
+  """
+  """
+
+  ##
+  # @if jp
+  # @brief コンストラクタ
+  #
+  # コンストラクタ
+  #
+  # @param self
+  #
+  # @else
+  # @brief Constructor
+  #
+  # @param self
+  #
+  # @endif
+  def __init__(self):
+    pass
+
+  ##
+  # @if jp
+  # @brief デストラクタ
+  #
+  #
+  # @param self
+  #
+  # @else
+  #
+  # @brief self
+  #
+  # @endif
+  def __del__(self):
+    pass
+
+  ##
+  # @if jp
+  # @brief 設定初期化
+  #
+  # 
+  # @param prop 設定情報
+  #
+  # @else
+  #
+  # @brief Initializing configuration
+  #
+  #
+  # @param prop Configuration information
+  #
+  # @endif
+  ## virtual ReturnCode init(coil::Properties& prop) = 0;
+  def init(self, prop):
+    pass
+
+
+  ##
+  # @if jp
+  # @brief データの変換(omniORB->ROS2)
+  #
+  # 
+  # @param data omniORBのデータ
+  # @return ret、value
+  # ret：SERIALIZE_OK：成功、SERIALIZE_ERROR：失敗、SERIALIZE_NOTFOUND：指定のシリアライザがない
+  # value：ROS2のデータ
+  #
+  # @else
+  #
+  # @brief 
+  #
+  #
+  # @param data 
+  # @return
+  #
+  # @endif
+  def serialize(self, data):
+    msg = Image()
+    msg.header.stamp.secs = data.tm.sec
+    msg.header.stamp.nsecs = data.tm.nsec
+    msg.height = data.height
+    msg.width = data.width
+    if not data.format:
+      msg.encoding = "rgb8"
+    else:
+      msg.encoding = data.format
+    msg.step = 1920
+    msg.data = data.pixels
+
+    return OpenRTM_aist.ByteDataStreamBase.SERIALIZE_OK, msg
+
+  ##
+  # @if jp
+  # @brief データの変換(ROS2->omniORB)
+  #
+  # @param self 
+  # @param bdata ROS2のデータ
+  # @param data_type omniORBのデータ
+  # @return ret、value
+  # ret：SERIALIZE_OK：成功、SERIALIZE_ERROR：失敗、SERIALIZE_NOTFOUND：指定のシリアライザがない
+  # value：変換後のデータ
+  #
+  # @else
+  #
+  # @brief 
+  #
+  # @param cdr
+  # @param data_type 
+  # @return 
+  #
+  # @endif
+  def deserialize(self, bdata, data_type):
+    try:
+      data_type.tm.sec = bdata.header.stamp.secs
+      data_type.tm.nsec = bdata.header.stamp.nsecs
+      data.height = bdata.height
+      data_type.width = bdata.width
+      data_type.format = bdata.encoding 
+      data_type.pixels = bdata.data
+      return OpenRTM_aist.ByteDataStreamBase.SERIALIZE_OK, data_type
+    except:
+      return OpenRTM_aist.ByteDataStreamBase.SERIALIZE_NOT_SUPPORT_ENDIAN, data_type
+
+
+##
+# @if jp
+# @brief Image型のシリアライザの初期化
+#
+#
+# @else
+# @brief 
+#
+#
+# @endif
+#
+def ROS2CameraImageInit():
+  OpenRTM_aist.SerializerFactory.instance().addFactory("ROS2Image",
+                                                      ROS2CameraImageData,
+                                                      OpenRTM_aist.Delete)
+  ROS2MessageInfo.ROS2MessageInfoFactory.instance().addFactory("ROS2Image",
+                                                      ROS2MessageInfo.ros2_message_info(Image),
+                                                      OpenRTM_aist.Delete)
+
+
+##
+# @if jp
+# @brief 各種シリアライザの初期化関数
+#
+#
+# @else
+# @brief 
+#
+#
+# @endif
+#
+def ROS2SerializerInit():
+  ROS2BasicDataInit(Float32, "ROS2Float32")
+  ROS2BasicDataInit(Float64, "ROS2Float64")
+  ROS2BasicDataInit(Int8, "ROS2Int8")
+  ROS2BasicDataInit(Int16, "ROS2Int16")
+  ROS2BasicDataInit(Int32, "ROS2Int32")
+  ROS2BasicDataInit(Int64, "ROS2Int64")
+  ROS2BasicDataInit(UInt8, "ROS2UInt8")
+  ROS2BasicDataInit(UInt16, "ROS2UInt16")
+  ROS2BasicDataInit(UInt32, "ROS2UInt32")
+  ROS2BasicDataInit(UInt64, "ROS2UInt64")
+  ROS2BasicDataInit(String, "ROS2String")
+
+  ROS2BasicDataInit(Float32MultiArray, "ROS2Float32MultiArray")
+  ROS2BasicDataInit(Float64MultiArray, "ROS2Float64MultiArray")
+  ROS2BasicDataInit(Int8MultiArray, "ROS2Int8MultiArray")
+  ROS2BasicDataInit(Int16MultiArray, "ROS2Int16MultiArray")
+  ROS2BasicDataInit(Int32MultiArray, "ROS2Int32MultiArray")
+  ROS2BasicDataInit(Int64MultiArray, "ROS2Int64MultiArray")
+  ROS2BasicDataInit(UInt8MultiArray, "ROS2UInt8MultiArray")
+  ROS2BasicDataInit(UInt16MultiArray, "ROS2UInt16MultiArray")
+  ROS2BasicDataInit(UInt32MultiArray, "ROS2UInt32MultiArray")
+  ROS2BasicDataInit(UInt64MultiArray, "ROS2UInt64MultiArray")
+
+  ROS2Point3DInit()
+  ROS2QuaternionInit()
+  ROS2Vector3DInit()
+  ROS2CameraImageInit()
