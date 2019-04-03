f3e8201a0b72e1cba2636184f3552244df866308
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2019-04-03 16:34:42 +0900

    [compat] add installation file

--- setup.py
+++ setup.py
@@ -196,6 +196,9 @@ openrtm_ext_packages = [
   "OpenRTM_aist.ext.transport.ROSTransport",
   "OpenRTM_aist.ext.transport.ROS2Transport",
   "OpenRTM_aist.ext.transport.OpenSplice",
+  "OpenRTM_aist.ext.transport.local_service.nameservice_file",
+  "OpenRTM_aist.ext.extended_fsm",
+  "OpenRTM_aist.ext.fsm4rtc_observer",
   ]
 openrtm_utils_packages = [
   "OpenRTM_aist.utils",
@@ -223,7 +226,8 @@ baseidl_files = [
   "../ext/sdo/observer/ComponentObserver.idl",
   "ExtendedFsmService.idl",
   "DataPort_OpenRTM.idl",
-  "CSPPort.idl"
+  "CSPPort.idl",
+  "../ext/fsm4rtc_observer/ComponentObserver.idl"
   ]
 baseidl_mods  = ["RTM", "RTC", "SDOPackage", "OpenRTM"]
 baseidl_path  = os.path.normpath(current_dir + "/" + baseidl_dir)
