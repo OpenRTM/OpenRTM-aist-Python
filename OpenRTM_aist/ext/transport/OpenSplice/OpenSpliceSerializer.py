9ed6e0552917332bbe91364741095df6dd8418ca
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2019-04-08 15:38:11 +0900

    [compat] add __init__.py

--- OpenRTM_aist/ext/transport/OpenSplice/OpenSpliceSerializer.py
+++ OpenRTM_aist/ext/transport/OpenSplice/OpenSpliceSerializer.py
@@ -285,13 +285,7 @@ def OpenSpliceSerializerInit():
                                                       OpenSpliceSerializer,
                                                       OpenRTM_aist.Delete)
 
-  site_dirs = site.getsitepackages()
-  OpenRTM_dir = ""
-  for site_dir in site_dirs:
-    d = os.path.join(site_dir, "OpenRTM_aist")
-    if os.path.exists(d):
-      OpenRTM_dir = d
-      break
+  OpenRTM_dir = OpenRTM_aist.__path__[0]
 
   idl_dir = os.path.join(OpenRTM_dir, "RTM_IDL")
   basicdatatypefile = os.path.join(idl_dir, "BasicDataType.idl")
