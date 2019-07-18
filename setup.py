df66c029c66d4fde6ebc03836f220a9a30f5e00f
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2019-07-18 14:00:49 +0900

    add idl files

--- setup.py
+++ setup.py
@@ -231,7 +231,11 @@ baseidl_files = [
   "ExtendedFsmService.idl",
   "DataPort_OpenRTM.idl",
   "CSPPort.idl",
-  "../ext/fsm4rtc_observer/ComponentObserver.idl"
+  "../ext/fsm4rtc_observer/ComponentObserver.idl",
+  "CameraCommonInterface.idl",
+  "ManipulatorCommonInterface_Common.idl",
+  "ManipulatorCommonInterface_DataTypes.idl",
+  "ManipulatorCommonInterface_Middle.idl"
   ]
 baseidl_mods  = ["RTM", "RTC", "SDOPackage", "OpenRTM"]
 baseidl_path  = os.path.normpath(current_dir + "/" + baseidl_dir)
