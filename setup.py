ac0a5324829f9d26e4a2a374155b9b9b076a90ea
n-kawauchi <n.kawauchi@aist.go.jp>
2020-07-07 17:02:41 +0900

    Added the extension js to the document.

--- setup.py
+++ setup.py
@@ -294,7 +294,7 @@ example_path = os.path.normpath(current_dir + "/" + example_dir)
 #
 document_dir = "OpenRTM_aist/docs"
 target_doc_dir = "share/openrtm-" + pkg_shortver + "/doc/python"
-document_match_regex = r".*\.(css|gif|png|html||hhc|hhk|hhp)$"
+document_match_regex = r".*\.(css|gif|png|html||hhc|hhk|hhp|js)$"
 document_path = os.path.normpath(current_dir + "/" + document_dir)
 
 
